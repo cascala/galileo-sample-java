178c5ef55cb8af2d3256bf0e7ed588bfb30f19bf
cascala <cascala@users.noreply.github.com>
2018-04-06 16:36:40 -0400

    Fixed formatting in Main

--- src/main/java/com/github/cascala/Main.java
+++ src/main/java/com/github/cascala/Main.java
@@ -1,19 +1,19 @@
 public class Main {
 	public static void main( String[] args ) {
 		// ExprHandler example
-    	ExprHandlerSample exprHandlerSample = new ExprHandlerSample();
-    	exprHandlerSample.run();
+		ExprHandlerSample exprHandlerSample = new ExprHandlerSample();
+		exprHandlerSample.run();
 
-    	// Linear Algebra example
-    	LinAlgSample linAlgSample = new LinAlgSample();
-    	linAlgSample.run();
+		// Linear Algebra example
+		LinAlgSample linAlgSample = new LinAlgSample();
+		linAlgSample.run();
 
 		// Number example
-    	NumberSample numberSample = new NumberSample();
-    	numberSample.run();
+		NumberSample numberSample = new NumberSample();
+		numberSample.run();
 
 		// Parser example
 		ParserSample parserSample = new ParserSample();
-    	parserSample.run();
-    }
+		parserSample.run();
+	}
 }
