01174251fab779f6038492debfae2df0f3517b64
cascala <cascala@users.noreply.github.com>
2018-04-08 10:11:58 -0400

    Dealing with Java to Scala collection incompatibilities

--- src/main/java/com/github/cascala/LinAlgSample.java
+++ src/main/java/com/github/cascala/LinAlgSample.java
@@ -1,10 +1,23 @@
 import galileo.linalg.DenseMatrix;
+import galileo.expr.Expr;
+import galileo.expr.Number;
 
-import scala.collection.immutable.List;
+import java.util.ArrayList;
+
+import scala.collection.JavaConverters;
 
 class LinAlgSample {
 	public void run(){
 		System.out.println( "Running " + getClass().getName() + ".run()"  );
-		//DenseMatrix m = new DenseMatrix();
+		// ArrayList constructor syntax is not very conducive to terse implementations
+		//DenseMatrix m = new DenseMatrix( [
+		ArrayList row = new ArrayList<Expr>(); //[ new Number( 1 ); new Number( 2 ); ];	
+		row.add( new Number( 1 ) );
+		row.add( new Number( 2 ) );
+		ArrayList matrix = new ArrayList<ArrayList<Expr>>();
+		matrix.add( row );
+		//               asScalaIterableConverter
+		//JavaConverters.asScalaIterableConverter( matrix.iterator() ); //.asScala();
+		//DenseMatrix denseMatrix = new DenseMatrix( matrix );
 	}
 }
